3c4a1e4bfbcdf99d2897bb29bfc4572302aaf3c6
miurary <ryantmiura@gmail.com>
2026-06-29 09:14:07 -0700

    last eslint fixes, ignore one line since emittedEvent args are unknown type array

--- wiggles/test/socket-handlers.test.ts
+++ wiggles/test/socket-handlers.test.ts
@@ -18,6 +18,7 @@ const connect = (h: Harness, id: string): FakeSocket => {
   return h.socket(id);
 };
 
+// eslint-disable-next-line
 const lastOf = (s: FakeSocket, event: string): any => {
   const es = s.emittedOf(event);
   return (es[es.length - 1] as EmittedEvent | undefined)?.args[0];
